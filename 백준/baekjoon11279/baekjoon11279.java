4bbb4fdcb7d91d348d24a02a44d39077e7ee4c2e
오승훈 <shoon0728@naver.com>
2025-03-20 23:36:29 +0900

    백준 1927, 11279 문제 추가

---
+++ 백준/baekjoon11279/baekjoon11279.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i = 0 ; i < N; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                if(q.isEmpty())
+                    System.out.println(0);
+                else{
+                    System.out.println(q.poll());
+                }
+                    
+            }
+
+            else{
+                q.add(num);
+            }
+
+        }
+    }
+}
